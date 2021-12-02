f7b4e38dc8908d08f185a1fe06f394dc695b34fe
jenniferstrand <95365822+jenniferstrand@users.noreply.github.com>
2021-12-02 14:20:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,88 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { TextField } from "@material-ui/core";
+import Checkbox from '@mui/material/Checkbox';
 
+
+// custom files
 import CustomHeader from "./CustomHeader";
+
+// set some vars
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 
 export default function App() {
-
 
   return (
     <div>
 
       <CustomHeader />
+
       <Typography variant="h5">Registrer ny bruker</Typography>
       <TextField
           style={{ width: "200px", margin: "5px" }}
           type="text"
-          label="setgoal"
+          label="id"
           variant="outlined"
         />
+        <br />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="navn"
+          variant="outlined"
+        />
+        <br />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="adresse"
+          variant="outlined"
+        />
+        <br />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="postnr"
+          variant="outlined"
+        />
+        <br />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="poststed"
+          variant="outlined"
+        />
+        <br />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="telefon"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="epost"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="fødselsdato"
+          variant="outlined"
+        />
+        <br />
+        Tillat-markedsforing ?
+        <Checkbox {...label} defaultChecked />
+
         <br />
 <form>
 
